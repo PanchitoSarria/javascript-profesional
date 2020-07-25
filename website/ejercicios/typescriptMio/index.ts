fdf48f54e4a2e68b270b6327403c8c35bbe0789c
Franciscco Andres Sarría <franasarria@gmail.com>
2020-07-25 19:40:25 -0300

    Commit antes de pasar parte del proyecto (o todo) a TypeScript

--- website/ejercicios/typescriptMio/index.ts
+++ website/ejercicios/typescriptMio/index.ts
@@ -91,6 +91,46 @@ console.log(interface1)
 
 
 
+
+interface SquareConfig {
+    color?: string;
+    width?: number;
+  }
+  
+  function createSquare(config: SquareConfig): { color: string; area: number } {
+    let newSquare = { color: "white", area: 100 };
+    if (config.color) {
+      newSquare.color = config.color;
+    }
+    if (config.width) {
+      newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+  }
+  
+  let mySquare = createSquare({ color: "black" });
+  console.log(mySquare)
+
+
+
+
+// Interfaces with Functions
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+  }
+
+let mySearch: SearchFunc = function (src, subString) {
+  let result = src.search(subString);
+  return result > -1;
+};
+console.log(mySearch('algo', 'al'))
+
+
+
+
+
+
+
 let fullName2: string = `Bob Bobbington`;
 let age: number = 37;
 let sentence: string = `Hello, my name is ${fullName2}.
@@ -134,3 +174,108 @@ interface ClockConstructor {
 
   console.log(digital)
   console.log(analog)
+
+
+
+
+
+
+  class Student {
+    fullName: string;
+    constructor(
+      public firstName: string,
+      public middleInitial: string,
+      public lastName: string
+    ) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+  }
+  
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+  
+  function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+  }
+  
+  let user = new Student("Jane", "M.", "User");
+  
+//   document.body.textContent = greeter(user);
+
+
+
+
+class Control {
+    private state: any;
+  }
+  
+  interface SelectableControl extends Control {
+    select(): void;
+  }
+  
+  class Button extends Control implements SelectableControl {
+    select() {}
+  }
+  
+  class TextBox extends Control {
+    select() {}
+  }
+  
+  // class ImageControl implements SelectableControl {
+  //   private state2: any;
+  //   select() {}
+  // }
+
+
+
+
+
+  // class Octopus {
+  //   readonly name: string;
+  //   readonly numberOfLegs: number = 8;
+  //   constructor(theName: string) {
+  //     this.name = theName;
+  //   }
+  // }
+  class Octopus {
+    readonly numberOfLegs: number = 8;
+    constructor(readonly name: string) {}
+  }
+  let dad = new Octopus("Man with the 8 strong legs");
+  // dad.name = "Man with the 3-piece suit";
+  console.log(dad)
+
+
+
+  enum LogLevel {
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG
+  }
+  
+  /**
+   * This is equivalent to:
+   * type LogLevelStrings = 'ERROR' | 'WARN' | 'INFO' | 'DEBUG';
+   */
+  type LogLevelStrings = keyof typeof LogLevel;
+  
+  function printImportant(key: LogLevelStrings, message: string) {
+    const num = LogLevel[key];
+    if (num <= LogLevel.WARN) {
+      console.log("Log level key is:", key);
+      console.log("Log level value is:", num);
+      console.log("Log level message is:", message);
+    }
+  }
+  printImportant("ERROR", "This is a message");
+
+  const enum Enum {
+    A = 1,
+    B = A * 2
+  }
+  
+  let uno = Enum.B
+  console.log(uno)
